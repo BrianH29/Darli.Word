397e73be7e2c809529207f32470d6137631d8338
Brian Hwang <brianhwang29@gmail.com>
2024-09-04 16:36:15 +0900

    enum Login = 0

---
+++ DataModels/ApplicationPage.cs
@@ -0,0 +1,13 @@
+﻿namespace Darli.Word
+{
+    /// <summary>
+    /// A Page of the application
+    /// </summary>
+    public enum ApplicationPage
+    {
+        /// <summary>
+        /// Initial Login Page
+        /// </summary>
+        Login = 0,
+    }
+}

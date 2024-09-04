96dd3539d703e11cc0fed5c5fd466e152dbb091f
Brian Hwang <brianhwang29@gmail.com>
2024-09-04 16:35:36 +0900

    Login Page created

---
+++ Pages/LoginPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Darli.Word.pages
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
